dbcd55a6c18e7243fd51020a21e089357187fc7b
rattri <bisnis.ratri@gmail.com>
2016-12-10 20:40:27 +0700

    add gps

--- app/src/main/java/solfa/ratri/kegiatan/Fragment/FragmentLokasi.java
+++ app/src/main/java/solfa/ratri/kegiatan/Fragment/FragmentLokasi.java
@@ -7,9 +7,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import solfa.ratri.kegiatan.Adapter.AlamatAdapter;
 import solfa.ratri.kegiatan.FourSquare.ModelApi;
+import solfa.ratri.kegiatan.GPSTracker;
 import solfa.ratri.kegiatan.R;
 
 /**
@@ -19,6 +21,8 @@ public class FragmentLokasi extends BaseFragment {
     RecyclerView recyclerView;
     AlamatAdapter adapter;
     LinearLayoutManager layoutManager;
+    GPSTracker gps;
+    public double longitude, latitude;
 
     public FragmentLokasi() {
     }
@@ -26,7 +30,24 @@ public class FragmentLokasi extends BaseFragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        adapter = new AlamatAdapter(getBaseActivity(), (FragmentAdd)getTargetFragment(), "VOWLDU5UW1MU4TK50V4GKK1315AQL5TO3LUKE2NS1ZI1HOGG", "Z0ONVIMP2TL5U2AA3O1RAFKCK2C5VYNYLMWW45R5UGLTIP2N", "400", "20130815", "-6.9213855,107.6216999");
+        // create class object
+        gps = new GPSTracker(getBaseActivity());
+
+        // check if GPS enabled
+        if(gps.canGetLocation()){
+
+           latitude = gps.getLatitude();
+           longitude = gps.getLongitude();
+
+            // \n is for new line
+            //Toast.makeText(getBaseActivity(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+        }else{
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gps.showSettingsAlert();
+        }
+        adapter = new AlamatAdapter(getBaseActivity(), (FragmentAdd)getTargetFragment(), "VOWLDU5UW1MU4TK50V4GKK1315AQL5TO3LUKE2NS1ZI1HOGG", "Z0ONVIMP2TL5U2AA3O1RAFKCK2C5VYNYLMWW45R5UGLTIP2N", "400", "20130815", latitude+","+longitude);
         adapter.init();
         layoutManager = new LinearLayoutManager(getContext());
     }
